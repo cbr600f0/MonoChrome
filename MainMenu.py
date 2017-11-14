7abe05bf7ed1d5734d62c9525968a5746e385a77
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2017-11-14 15:24:58 +0100

    Changed main menu

--- MainMenu.py
+++ MainMenu.py
@@ -2,24 +2,22 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((1980, 1080), FULLSCREEN)
-done = False
+screen = pygame.display.set_mode((1920, 1080), FULLSCREEN)
+gameIsRunning = True
 
 clock = pygame.time.Clock()
 
-while not done:
+while gameIsRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
+            gameIsRunning = False
 
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: y -= 3
-    if pressed[pygame.K_DOWN]: y += 3
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        gameIsRunning = False
+
+    keysPressed = pygame.key.get_pressed()
 
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
 
     pygame.display.flip()
     clock.tick(60)
