01281a170ec92bcc71a08665e55bea9a885c773f
Reacryoum <0947683@hr.nl>
2017-11-21 13:50:48 +0100

    Updated pong

--- Pong/PongScene.py
+++ Pong/PongScene.py
@@ -12,12 +12,16 @@ class PongScene (Scene):
 
     def render(self, screen):
         screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(self.x, self.y, 60, 240))
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(self.x, self.y, 60, 240)) #paddle
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1540, 0, 60, 400)) #backwall
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1540, 500, 60, 900)) #backwall
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(720, 390, 60, 60)) #ball
 
     def update(self, deltaTime):
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]: self.y -= 200 * deltaTime
         if pressed[pygame.K_DOWN]: self.y += 200 * deltaTime
+
 
     def handle_events(self, events):
         pass
