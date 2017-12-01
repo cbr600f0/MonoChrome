87cf1de7f16a75aa488269b28d814dd96744c81a
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2017-12-01 20:26:00 +0100

    Changed Scenemanger and TowerDefense

--- GameLoop.py
+++ GameLoop.py
@@ -1,7 +1,7 @@
 import pygame
 from ButtonClass import Button
 from pygame.locals import *
-import SceneManager # This class takes care of switching between scenes (Examples of scene could be: the main menu, Tower Defense, Pong, ETC) Every game is its own scene (For badbois who already have experience with Unity the concept of a scene is the same here as in Unity)
+from SceneManager import SceneMananger  # This class takes care of switching between scenes (Examples of scene could be: the main menu, Tower Defense, Pong, ETC) Every game is its own scene (For badbois who already have experience with Unity the concept of a scene is the same here as in Unity)
 
 
 # Starts the game by initializing pygame
@@ -11,7 +11,6 @@ gameIsInFullscreen = True  # Change this to False if you want to make the screen
 if gameIsInFullscreen == False:
     # Sets to screen size to a specified size
     screen = pygame.display.set_mode((1600, 900))
-    switchScreenButton.text = "Windowed"
 else:
     # Sets to screen size to a specified size and makes the screen fullscreen
     screen = pygame.display.set_mode((1600, 900), pygame.FULLSCREEN)
@@ -23,7 +22,7 @@ gameIsRunning = True
 clock = pygame.time.Clock()
 
 # Tells the SceneManger to go to the MainMenu Scene (Yes seeing SceneManager.SceneMananger is weird im still trying to understand how i can fix this to be only SceneManager.goToScene(""))
-SceneManager.SceneMananger.goToScene("MonoChromeIntroScene")
+SceneMananger.goToScene("MonoChromeIntroScene")
 
 # Time that has passed since the last frame
 deltaTime = 0
@@ -51,9 +50,9 @@ while gameIsRunning:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
             gameIsPaused = not gameIsPaused
 
-    SceneManager.SceneMananger.currentScene.handle_events(allEvents)  #Handles the events of the currentScene (the currentScene is the scene wich is playing now)
-    SceneManager.SceneMananger.currentScene.update(deltaTime)  #Handles the updates of the currentScene (the currentScene is the scene wich is playing now)
-    SceneManager.SceneMananger.currentScene.render(screen)  #Handles the rendering of the currentScene (the currentScene is the scene wich is playing now)
+    SceneMananger.currentScene.handle_events(allEvents)  #Handles the events of the currentScene (the currentScene is the scene wich is playing now)
+    SceneMananger.currentScene.update(deltaTime)  #Handles the updates of the currentScene (the currentScene is the scene wich is playing now)
+    SceneMananger.currentScene.render(screen)  #Handles the rendering of the currentScene (the currentScene is the scene wich is playing now)
 
     switchScreenButton.draw(screen)
     if switchScreenButton.click():
--- TowerDefense/TowerDefenseMainMenuScene.py
+++ TowerDefense/TowerDefenseMainMenuScene.py
@@ -12,17 +12,17 @@ class TowerDefenseMainMenuScene(Scene):
 
         self.mainBG = pygame.image.load("TowerDefense\Images\MainMenu.png").convert()
         self.mainBG = pygame.transform.scale(self.mainBG, (1600, 900))
-        self.backToMainMenuBtn = Button("Start", None, None, [120, 120, 120], [132, 115, 100], 715, 195, None, 70)
+        self.startBtn = Button("Start", None, None, [120, 120, 120], [132, 115, 100], 715, 195, None, 70)
 
     def render(self, screen):
         screen.blit(self.mainBG, (0, 0))
 
-        self.backToMainMenuBtn.draw(screen)
+        self.startBtn.draw(screen)
 
     def update(self, deltaTime):
 
-        if self.backToMainMenuBtn.click():
-            SceneManager.SceneMananger.goToScene("MainMenu")
+        if self.startBtn.click():
+            SceneManager.SceneMananger.goToScene("TDLevel1")
 
     def handle_events(self, events):
         for event in events:
