b60982ea6d1e665d2c443e20ee3f126e02b55668
Reacryoum <0947683@hr.nl>
2017-12-05 11:56:08 +0100

    Updated pong

--- Pong/PongScene.py
+++ Pong/PongScene.py
@@ -13,17 +13,21 @@ class PongScene (SceneManager.Scene):
 
     def render(self, screen):
         screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(self.player1x, self.player1y, 60, 240)) #player 1
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(self.player1x, self.player1y, 30, 180)) #player 1
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(720, 390, 60, 60)) #ball
-        pygame.draw.rect(screen,[255, 255, 255], pygame.Rect(self.player2x, self.player2y, 60, 240))   #player 2
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(self.player2x, self.player2y, 30, 180))   #player 2
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(0, 45, 1600, 5)) #barrier, scoreboard border
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(395, 0, 5, 45))  #scoreboard border
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(795, 0, 5, 45))  # scoreboard border
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1195, 0, 5, 45))  # scoreboard border
 
 
     def update(self, deltaTime):
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_w]: self.player1y -= 200 * deltaTime    #player 1 movement up
-        if pressed[pygame.K_s]: self.player1y += 200 * deltaTime    #player 1 movement down
-        if pressed[pygame.K_UP]: self.player2y -= 200 * deltaTime   #player 2 movement up
-        if pressed[pygame.K_DOWN]: self.player2y += 200 * deltaTime #player 2 movement down
+        if pressed[pygame.K_w]: self.player1y -= 300 * deltaTime    #player 1 movement up
+        if pressed[pygame.K_s]: self.player1y += 300 * deltaTime    #player 1 movement down
+        if pressed[pygame.K_UP]: self.player2y -= 300 * deltaTime   #player 2 movement up
+        if pressed[pygame.K_DOWN]: self.player2y += 300 * deltaTime #player 2 movement down
 
 
     def handle_events(self, events):
