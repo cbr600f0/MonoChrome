afd1f0309d76091c6e3be90ec955764d1825613f
Reacryoum <0947683@hr.nl>
2017-12-05 15:06:02 +0100

    Pong updated

--- Pong/PongMainMenuScene.py
+++ Pong/PongMainMenuScene.py
@@ -1,11 +1,14 @@
 import pygame
 import SceneManager
 from ButtonClass import Button
+from Pong import PongMultiplayerScene
 
 class PongMainMenuScene (SceneManager.Scene):
 
     def __init__(self):
         super(PongMainMenuScene, self).__init__()
+        self.MultiplayerButton = Button("Multiplayer", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 200, 200, 1300, 250)
+
 
     def render(self, screen):
         screen.fill((0, 0, 0))
@@ -13,11 +16,12 @@ class PongMainMenuScene (SceneManager.Scene):
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1540, 0, 60, 60))     #decoration
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(0, 840, 60, 60))      #decoration
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1540, 840, 60, 60))   #decoration
-        
+        self.MultiplayerButton.draw(screen)
 
     def update(self, deltaTime):
         pressed = pygame.key.get_pressed()
-
+        if(self.MultiplayerButton.click()):
+            SceneManager.SceneMananger.currentScene = PongMultiplayerScene.PongMultiplayerScene()
 
     def handle_events(self, events):
         pass
