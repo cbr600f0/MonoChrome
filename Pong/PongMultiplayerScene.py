6597df677b83f6d9753a2838ebd396f1d1176b44
Reacryoum <0947683@hr.nl>
2017-12-06 14:36:24 +0100

    Pong updated

--- Pong/PongMultiplayerScene.py
+++ Pong/PongMultiplayerScene.py
@@ -1,6 +1,7 @@
 import pygame
 import SceneManager
 from ButtonClass import Button
+from Pong import Ball
 
 class PongMultiplayerScene (SceneManager.Scene):
 
@@ -14,7 +15,7 @@ class PongMultiplayerScene (SceneManager.Scene):
     def render(self, screen):
         screen.fill((0, 0, 0))
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(self.player1x, self.player1y, 30, 180)) #player 1
-        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(720, 390, 60, 60)) #ball
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(720, 390, 60, 60))  # ball
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(self.player2x, self.player2y, 30, 180))   #player 2
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(0, 45, 1600, 5)) #barrier, scoreboard border
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(395, 0, 5, 45))  #scoreboard border
@@ -24,19 +25,24 @@ class PongMultiplayerScene (SceneManager.Scene):
 
     def update(self, deltaTime):
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_w]: self.player1y -= 400 * deltaTime    #player 1 movement up
-        if pressed[pygame.K_s]: self.player1y += 400 * deltaTime    #player 1 movement down
+        if pressed[pygame.K_w]:
+            self.player1y -= 500 * deltaTime    #player 1 movement up
+        if pressed[pygame.K_s]:
+            self.player1y += 500 * deltaTime    #player 1 movement down
         if self.player1y < 50:
             self.player1y = 50
         if self.player1y > 720:
             self.player1y = 720
-        if pressed[pygame.K_UP]: self.player2y -= 400 * deltaTime   #player 2 movement up
-        if pressed[pygame.K_DOWN]: self.player2y += 400 * deltaTime #player 2 movement down
+        if pressed[pygame.K_UP]:
+            self.player2y -= 500 * deltaTime   #player 2 movement up
+        if pressed[pygame.K_DOWN]:
+            self.player2y += 500 * deltaTime #player 2 movement down
         if self.player2y < 50:
             self.player2y = 50
         if self.player2y > 720:
             self.player2y = 720
-
+        if pressed[pygame.K_BACKSPACE]:
+            SceneManager.SceneManager.goToScene("Pong.PongMainMenuScene.PongMainMenuScene")
 
     def handle_events(self, events):
         pass
