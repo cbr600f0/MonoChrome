096e08a5413bb7f6e53423bfc90995bc6740ad9d
Reacryoum <0947683@hr.nl>
2017-12-06 14:56:22 +0100

    Pong updated

--- Pong/PongMainMenuScene.py
+++ Pong/PongMainMenuScene.py
@@ -7,24 +7,28 @@ class PongMainMenuScene (SceneManager.Scene):
 
     def __init__(self):
         super(PongMainMenuScene, self).__init__()
-        self.SinglePlayerButton = Button("Single player", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 400, 25, 800, 150)
-        self.MultiplayerButton = Button("Multiplayer", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 400, 200, 800, 150)
-        self.InfiniteButton = Button("Infinite", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 400, 375, 800, 150)
-        self.AudioButton = Button("Audio", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 400, 550, 800, 150)
-        self.ExitButton = Button("Exit", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 400, 725, 800, 150)
-
+        self.SinglePlayerButton = Button("Single player", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 600, 225, 400, 75)
+        self.MultiplayerButton = Button("Multiplayer", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 600, 325, 400, 75)
+        self.InfiniteButton = Button("Infinite", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 600, 425, 400, 75)
+        self.AudioButton = Button("Audio", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 600, 525, 400, 75)
+        self.ExitButton = Button("Exit", (255, 255, 255), (255, 255, 255), (255, 255, 255), (0, 0, 0), 600, 625, 400, 75)
+        self.myfont = pygame.font.SysFont("monospace", 72)
 
     def render(self, screen):
         screen.fill((0, 0, 0))
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(0, 0, 60, 60))        #decoration
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1540, 0, 60, 60))     #|
         pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(0, 840, 60, 60))      #|
-        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1540, 840, 60, 60))   #decoration
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1540, 840, 60, 60))   #|
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(60, 300, 60, 360))    #|
+        pygame.draw.rect(screen, [255, 255, 255], pygame.Rect(1480, 300, 60, 360))  #decoration
         self.SinglePlayerButton.draw(screen)                                        #button
         self.MultiplayerButton.draw(screen)                                         #|
         self.InfiniteButton.draw(screen)                                            #|
         self.AudioButton.draw(screen)                                               #|
         self.ExitButton.draw(screen)                                                #button
+        label = self.myfont.render("PONG", 1, (255, 255, 255))
+        screen.blit(label, (700, 100))
 
     def update(self, deltaTime):
         pressed = pygame.key.get_pressed()
