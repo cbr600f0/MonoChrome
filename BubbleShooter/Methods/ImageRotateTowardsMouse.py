57d2da1def08dc82bddce5924619eeb5b9cab427
cbr600f0 <sidneybatenburg@hotmail.com>
2017-12-12 11:03:57 +0100

    Hover over a button now shows the right color

--- BubbleShooter/Methods/ImageRotateTowardsMouse.py
+++ BubbleShooter/Methods/ImageRotateTowardsMouse.py
@@ -2,7 +2,7 @@ import sys, pygame, math
 from pygame.locals import *
 
 class ImageRotateTowardsMouse():
-    def __init__(self, rotatingImage, screen, width = 0, height = 0, posX = 0, posY = 0):
+    def __init__(self, rotatingImage, screen, width=0, height=0, posX=0, posY=0):
         """
         :param rotatingImage holds the path towards the images you want to rotate:
         :param screen is needed to draw the image to the screen:
@@ -12,15 +12,9 @@ class ImageRotateTowardsMouse():
         :param posY, optional, sets the y-as of the center of the image (Note: 0 will make the CENTER of the y-as 0):
         """
         rotatingImage = pygame.image.load(rotatingImage).convert_alpha()
-        while True:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-                rotatingImage = pygame.transform.scale(rotatingImage, (width, height))
-                pos = pygame.mouse.get_pos()
-                angle = 360 - math.atan2(pos[1] - posY, pos[0] - posX) * 180 / math.pi
-                rotimage = pygame.transform.rotate(rotatingImage, angle)
-                rect = rotimage.get_rect(center=(posX, posY))
-                screen.blit(rotimage, rect)
-                pygame.display.update()
+        rotatingImage = pygame.transform.scale(rotatingImage, (width, height))
+        pos = pygame.mouse.get_pos()
+        angle = 360 - math.atan2(pos[1] - posY, pos[0] - posX) * 180 / math.pi
+        rotimage = pygame.transform.rotate(rotatingImage, angle)
+        rect = rotimage.get_rect(center=(posX, posY))
+        screen.blit(rotimage, rect)
