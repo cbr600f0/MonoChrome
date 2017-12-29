7e234227afa71fb4e25b59c7d7bc884c9396ac27
Griffin33 <33390937+Griffin33@users.noreply.github.com>
2017-12-29 19:59:11 +0100

    Added ball collision in DXBALL

--- DXBall/DXBallMainMenuScene.py
+++ DXBall/DXBallMainMenuScene.py
@@ -22,9 +22,9 @@ class DXBallMainMenuScene(SceneManager.Scene):
 
         self.buttonList = []
 
-        self.startBtn = Button(False, self.DXBallFont, "[Start]", None, None, [2, 255, 149], [2, 255, 149], 570, 490, None, 60)
-        self.optionsBtn = Button(False, self.DXBallFont , "[Options]", None, None, [2, 255, 149], [2, 255, 149], 570, 545, None, 60)
-        self.quitBtn = Button(False, self.DXBallFont, "[Quit]", None, None, [2, 255, 149], [2, 255, 149] , 570, 600, None, 60)
+        self.startBtn = Button(False, self.DXBallFont, "[Start]", None, None, [2, 255, 149], [2, 255, 149], 44, 540, None, 60)
+        self.optionsBtn = Button(False, self.DXBallFont , "[Options]", None, None, [2, 255, 149], [2, 255, 149], 44, 590, None, 60)
+        self.quitBtn = Button(False, self.DXBallFont, "[Quit]", None, None, [2, 255, 149], [2, 255, 149], 44, 640, None, 60)
 
         self.buttonList.append(self.startBtn)
         self.buttonList.append(self.optionsBtn)
