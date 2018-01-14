aca4e81f52decd5c2269531022446d369690a3d6
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2018-01-14 15:40:32 +0100

    nieuwe commit 7

--- MonoChromeIntroScene.py
+++ MonoChromeIntroScene.py
@@ -5,17 +5,21 @@ class MonoChromeIntroScene(SceneManager.Scene): # MainMenuScene inherits from th
 
     def __init__(self):
         super(MonoChromeIntroScene, self).__init__()  # get the methods and variables from the base class wich is Scene
+
+        self.backgroundImage = pygame.image.load("IntroImage.png").convert()
+
         self.headerFont = pygame.font.SysFont("monospace", 28)
         self.headerFont.set_bold(True)
 
     # The function of this method is explained in the class Scene
     def render(self, screen):
-        pygame.display.set_caption("MonoChrome intro (Press ESCAPE to close the game)")  # Changes the text of the window
-        screen.fill((30, 30, 30))
-
-        # render text
-        label = self.headerFont.render("PRESS SPACE TO START THE GAME", 1, (255, 255, 255))
-        screen.blit(label, (70, 20))
+        screen.blit(self.backgroundImage, (0, 0))
+        # pygame.display.set_caption("MonoChrome intro (Press ESCAPE to close the game)")  # Changes the text of the window
+        # screen.fill((30, 30, 30))
+        #
+        # # render text
+        # label = self.headerFont.render("PRESS SPACE TO START THE GAME", 1, (255, 255, 255))
+        # screen.blit(label, (70, 20))
 
     # The function of this method is explained in the class Scene
     def update(self, deltaTime):
